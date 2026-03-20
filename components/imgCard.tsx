09e0eb060c219bafd14ba76ca1d625c44bb26ec9
DanielVas <cruzavas@gmail.com>
2026-03-19 20:27:38 -0600

    Dev (#2)
    * Use useQuery, pages and layout
    
    * CharacterId view

--- components/imgCard.tsx
+++ components/imgCard.tsx
@@ -1,30 +1,35 @@
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+import { ICharacter } from '@/interfaces/ICharacter';
 
-const ImgCard = ({ name, specie, image }: { name: string, specie: string, image: string }) => {
+const ImgCard = ({ character }: { character : ICharacter }) => {
   return (
     <Card sx={{ maxWidth: 345, padding: 0 }}>
       <CardMedia
         component="img"
-        alt={ name }
+        alt={ character.name }
         height="300"
-        image={ image }
+        image={ character.image }
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          { name }
+          { character.name }
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          { specie }
+          { character.species }
         </Typography>
       </CardContent>
       <CardActions>
-        <Button variant="contained" size="medium" sx={{backgroundColor: 'orangered'}}>Learn More</Button>
+        <Link href={`/character/${character.id}`} style={{color: 'white', textDecoration: 'none'}}>
+         <Button variant="contained" size="medium" sx={{backgroundColor: 'orangered', '&:hover': {backgroundColor: 'white', color: 'orangered'}}}>
+          Learn More
+         </Button>
+        </Link>
       </CardActions>
     </Card>
   )
